a18810c3da0b39da916ba9cce42cdd89981dccf6
Jan Kubica <kubica.j88@gmail.com>
2023-05-29 21:48:29 +0100

    google login implemented

---
+++ Assets/Managers/GoogleLoginManager.cs
@@ -0,0 +1,41 @@
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+
+namespace Managers
+{
+    public class GoogleLogin : MonoBehaviour
+    {
+        public string Token;
+        public string Error;
+
+        void Awake()
+        {
+            //Initialize PlayGamesPlatform
+            PlayGamesPlatform.Activate();
+            LoginGooglePlayGames();
+        }
+
+        public void LoginGooglePlayGames()
+        {
+            PlayGamesPlatform.Instance.Authenticate((success) =>
+            {
+                if (success == SignInStatus.Success)
+                {
+                    Debug.Log("Login with Google Play games successful.");
+
+                    PlayGamesPlatform.Instance.RequestServerSideAccess(true, code =>
+                    {
+                        Debug.Log("Authorization code: " + code);
+                        Token = code;
+                    });
+                }
+                else
+                {
+                    Error = "Failed to retrieve Google play games authorization code";
+                    Debug.Log("Login Unsuccessful");
+                }
+            });
+        }
+    }
+}
