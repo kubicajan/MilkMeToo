b16634577d16eff2d7301837c0ec1a5cd324a67a
Jan Kubica <kubica.j88@gmail.com>
2024-04-19 14:11:29 +0200

    more jetel added

--- Assets/Objects/PassiveObjects/Jetel.cs
+++ Assets/Objects/PassiveObjects/Jetel.cs
@@ -1,15 +1,37 @@
 using Objects.Abstract;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Objects.PassiveObjects
 {
     public class Jetel : PassiveKokTreeObjects
     {
+        [SerializeField] private Image anotherJetel;
+        [SerializeField] private Image yetAnotherJetel;
+        [SerializeField] private Image yetAnotherAnotherJetel;
+
         public Jetel()
         {
             kokButtonStatus = ButtonStatus.AVAILABLE;
             objectName = "Jetel";
             kokButtonDescription = "Maybe it is time to start feeding your cows";
             kokButtonUnlockPrice = 5;
+        }
+        
+        protected override void Start()
+        {
+            base.Start();
+            anotherJetel.gameObject.SetActive(false);
+            yetAnotherJetel.gameObject.SetActive(false);
+            yetAnotherAnotherJetel.gameObject.SetActive(false);
+        }
+        
+        public override void BuyUpgrade()
+        {
+            base.BuyUpgrade();
+            anotherJetel.gameObject.SetActive(true);
+            yetAnotherJetel.gameObject.SetActive(true);
+            yetAnotherAnotherJetel.gameObject.SetActive(true);
         }
     }
 }
