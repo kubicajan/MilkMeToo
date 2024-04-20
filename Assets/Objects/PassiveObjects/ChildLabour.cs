cc0edefc450fb68732c9e552536312eba0a643d2
Jan Kubica <kubica.j88@gmail.com>
2024-04-20 15:58:28 +0200

    more children added

--- Assets/Objects/PassiveObjects/ChildLabour.cs
+++ Assets/Objects/PassiveObjects/ChildLabour.cs
@@ -1,14 +1,33 @@
 using Objects.Abstract;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Objects.PassiveObjects
 {
     public class ChildLabour : PassiveKokTreeObjects
     {
+        [SerializeField] private Button anotherSlave;
+        [SerializeField] private Button yetAnotherSlave;
         public ChildLabour()
         {
             objectName = "Child Labour";
             kokButtonDescription = "Let everyone help";
             kokButtonUnlockPrice = 5;
+        }
+        
+        protected override void Start()
+        {
+            base.Start();
+            anotherSlave.gameObject.SetActive(false);
+            yetAnotherSlave.gameObject.SetActive(false);
+        }
+        
+        public override void BuyUpgrade()
+        {
+            base.BuyUpgrade();
+            anotherSlave.gameObject.SetActive(true);
+            yetAnotherSlave.gameObject.SetActive(true);
         }
     }
 }
