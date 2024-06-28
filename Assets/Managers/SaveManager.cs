b61f0747ce87550a193b413a7c2556fe58ef4e5d
Jan Kubica <kubica.j88@gmail.com>
2024-06-28 13:51:21 +0200

    cow dimensions made smaller

--- Assets/Managers/SaveManager.cs
+++ Assets/Managers/SaveManager.cs
@@ -183,6 +183,8 @@ namespace Managers
                 timer = 0;
                 SaveGameData();
                 int million = 1000000;
+                
+                
                 if (GetTotalMoney() >= 999999999)
                 {
                     Social.ReportScore((999999999), GPGSIds.leaderboard_total_money_made_in_mil, success => { });
